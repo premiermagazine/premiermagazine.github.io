2b4c977c14ba9464dd5eb913de3a18e7abe45a79
Victor Kipchumba <viccicole11@gmail.com>
2023-10-07 01:26:35 +0300

    Coming soon optimization

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
 $(function () {
     var now = new Date();
-    var day = now.getDate();
-    var month = now.getMonth();
-    var year = now.getFullYear() + 1;
-
-    var nextyear = month + '/' + day + '/' + year + ' 07:07:07';
+    var nextweek = new Date(now.getTime() + 3 * 24 * 60 * 60 * 1000); // 7 days from now
 
     $('#timer').countdown({
-        date: nextyear, //TODO Date format: 07/27/2017 17:00:00
-        offset: +2,
+        date: nextweek, // Use the calculated next week date
         day: 'Day',
         days: 'Days'
     }, function () {
+        // Callback function when the countdown timer reaches zero
+        console.log("Countdown reached zero!");
     });
 });
